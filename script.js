3b4330dc629fde539175f43c5c9b69b3630e8c92
philipsoderbom <44798208+philipsoderbom@users.noreply.github.com>
2019-04-18 17:45:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const visaLogga = document.querySelector('.logo-hidden');
-
-$(document).ready(function() {
-	$(window).scroll(function() {
-		var posTop = $(document).scrollTop();
-		
-
-		if (posTop > 1070) {
-			visaLogga.classList.add('logo-visible');
-		}
-		else if (posTop < 1070) {
-			visaLogga.classList.remove('logo-visible');
-		}
-	})
-})
-
-
-
 const sidomeny = document.getElementById('sidomeny');
 const menyKnapp = document.getElementById('meny-knapp');
 
@@ -24,3 +6,65 @@ function visaMeny() {
 	sidomeny.classList.toggle('active');
 	menyKnapp.classList.toggle('active');
 }
+
+
+/*============showInfoLeft===============*/
+const showInfoButtonLeft = document.querySelector('.card-button-left');
+const hideImageLeft = document.querySelector('.img-wrapper#left');
+const showInfoLeft = document.querySelector('.hidden-info-card-left');
+
+
+showInfoButtonLeft.addEventListener('click', function(){
+	showInfoLeft.classList.toggle('showInfo');
+	hideImageLeft.classList.toggle('fadeOut');
+
+	var buttonLabel = document.querySelector(".card-button-left");
+	if (buttonLabel.innerHTML == "View Details") {
+		buttonLabel.innerHTML = "Hide Details";
+	} else {
+		buttonLabel.innerHTML = "View Details";
+	}
+
+})
+
+/*============showInfoRight===============*/
+const showInfoButtonRight = document.querySelector('.card-button-right');
+const hideImageRight = document.querySelector('.img-wrapper#right');
+const showInfoRight = document.querySelector('.hidden-info-card-right');
+
+
+showInfoButtonRight.addEventListener('click', function(){
+	showInfoRight.classList.toggle('showInfo');
+	hideImageRight.classList.toggle('fadeOut');
+
+
+	var buttonLabel2 = document.querySelector(".card-button-right");
+	if (buttonLabel2.innerHTML == "View Details") {
+		buttonLabel2.innerHTML = "Hide Details";
+	} else {
+		buttonLabel2.innerHTML = "View Details";
+	}
+})
+
+
+
+
+
+$(document).ready(function(){
+	$(window).scroll(function(event){
+		var y = $(this).scrollTop();
+		var count = 0;
+
+
+		if (y > 180) {
+			$('.left').addClass('scrollAnimation');
+			$('.right').addClass('scrollAnimation');
+			count++;
+		}
+		else {
+			$('.left').removeClass('scrollAnimation');
+			$('.right').removeClass('scrollAnimation');
+		}
+ 	})
+})
+
